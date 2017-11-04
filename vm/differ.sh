552931d6897ddeb61ac1b064b1d1f5f8bf197c69
Douglas ENGSTRAND <dengstra@e1r10p3.42.fr>
2017-11-04 20:05:02 +0100

    added scripts

---
+++ vm/differ.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+make
+rm -rf diff.txt
+rm -rf ft.txt
+rm -rf cp.txt
+
+path=~/corewar/champions/
+
+./asm $path$2.s
+./asm $path$3.s
+./asm $path$4.s
+./asm $path$5.s
+
+if [ $5 ]
+then
+	champs="$path$2.cor $path$3.cor $path$4.cor $path$5.cor"
+elif [ $4 ]
+then
+	champs="$path$2.cor $path$3.cor $path$4.cor"
+elif [ $3 ]
+then
+	champs="$path$2.cor $path$3.cor"
+elif [ $2 ]
+then
+	champs="$path$2.cor"
+fi
+
+diff <(./vm -v $1 $champs) <(~/corewar/software/corewar -v $1 $champs )
+#./vm -v $1 $champs > ft.txt
+#~/corewar/software/corewar -v $1 $champs > cp.txt 
+
+#diff ft.txt cp.txt > diff.txt
+#diff ft.txt cp.txt
---
+++ vm/fulltest.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+i=1
+while [ $i -le 16 ]
+do
+	#./differ.sh $i _ _ _ _
+	#./differ.sh $i champ champ champ champ
+	#./differ.sh $i turtle slider2 mortel littlepuppy
+	#./differ.sh $i Gagnant machine-gun fluttershy bigzork
+	#./differ.sh $i _honeybadger big_feet corelol champ
+	#./differ.sh $i champ gedeon helltrain mortel
+	#./differ.sh $i salamahenagalabadoun misaka_mikoto turtle turtle
+	#./differ.sh $i misaka_mikoto misaka_mikoto misaka_mikoto misaka_mikoto
+	#./differ.sh $i zork
+	#./differ.sh $i bigzork
+	#./differ.sh $i fluttershy
+	#./differ.sh $i helltrain
+	#./differ.sh $i turtle
+	#./differ.sh $i bee_gees
+	#./differ.sh $i _				# Error 2 and 8
+	#./differ.sh $i champ
+	#./differ.sh $i kappa
+	#./differ.sh $i marineking			# Error 2 and 8
+	#./differ.sh $i misaka_mikoto
+	#./differ.sh $i octobre_rouge_v4.2
+	#./differ.sh $i rainbow_dash
+	#./differ.sh $i thunder
+	#./differ.sh $i varimathras
+	#./differ.sh $i wall
+	#./differ.sh $i _honeybadger
+	#./differ.sh $i big_feet		#slow
+	#./differ.sh $i casimir
+	#./differ.sh $i champ
+	#./differ.sh $i corelol
+	#./differ.sh $i darksasuke
+	#./differ.sh $i doge
+	#./differ.sh $i dubo
+	#./differ.sh $i ex
+	#./differ.sh $i gedeon
+	#./differ.sh $i jumper 		# Error 2 and 8
+	#./differ.sh $i littlepuppy
+	#./differ.sh $i live
+	#./differ.sh $i loose
+	#./differ.sh $i machine-gun
+	#./differ.sh $i mandragore	#slow
+	#./differ.sh $i maxidef
+	#./differ.sh $i meowluigi 	#slow
+	#./differ.sh $i minus
+	#./differ.sh $i mortel
+	#./differ.sh $i rainbow_dash
+	#./differ.sh $i salamahenagalabadoun
+	#./differ.sh $i skynet
+	#./differ.sh $i slider2
+	#./differ.sh $i terminator
+	#./differ.sh $i thunder
+	#./differ.sh $i toto
+	#./differ.sh $i turtle
+	#./differ.sh $i ultimate-surrender
+	#./differ.sh $i varimathras
+	#./differ.sh $i wall
+	#./differ.sh $i youforkmytralala
+
+	((i = i * 2))
+done
+
